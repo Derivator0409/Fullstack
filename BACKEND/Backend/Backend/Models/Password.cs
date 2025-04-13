99a222f1cc1db4382875274cc79fac0e1617d41c
FZW0D1@stud.uni-obuda.hu <FZW0D1@stud.uni-obuda.hu>
2025-04-13 21:05:55 +0200

    Controllers és Services mappák és Model elkészítése

--- BACKEND/Backend/Backend/Models/Password.cs
+++ BACKEND/Backend/Backend/Models/Password.cs
@@ -2,5 +2,9 @@
 
 public class Password
 {
+    public string OriginalPassword { get; set; }
     
+    public string EncryptedPassword { get; set; }
+    
+    public int Complexity { get; set; }
 }
