e9e40526b186fee3c4a1be1519c2f3a91448b155
FZW0D1@stud.uni-obuda.hu <FZW0D1@stud.uni-obuda.hu>
2025-04-14 10:50:45 +0200

    PasswordController alapjának el készítése

---
+++ BACKEND/Backend/Backend/Controllers/PasswordController.cs
@@ -0,0 +1,26 @@
+﻿using Backend.Models;
+using Backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class PasswordController(IPasswordEncryptingService service):ControllerBase
+{
+    private readonly IPasswordEncryptingService _passwordEncryptingService=service;
+    [HttpPost]
+    public ActionResult<Password> Encrypt([FromBody] string password)
+    {
+        try
+        {
+            return Ok(_passwordEncryptingService.Encrypt(password));
+        }
+        catch (Exception )
+        {
+            return BadRequest();
+        }
+        
+    }
+}
