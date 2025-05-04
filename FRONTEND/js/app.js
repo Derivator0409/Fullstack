1ce4796f45ecbb367837ec30f2614336127d22ae
FZW0D1@stud.uni-obuda.hu <FZW0D1@stud.uni-obuda.hu>
2025-05-04 22:44:55 +0200

    application js elkészítése

--- FRONTEND/js/app.js
+++ FRONTEND/js/app.js
@@ -10,9 +10,49 @@ let options = {
   };
   
   fetch('https://localhost:7189/Password', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+  .then(response => response.json())
+  .catch(err => console.error(err))
+  .then(response => {
+
+    document.querySelector('#resultSection').classList.remove('d-none');
+  
+      document.querySelector('#originalPass').textContent = response.originalPassword;
+      document.querySelector('#encryptedPass').textContent = response.encryptedPassword;
+      document.querySelector('#complexity').textContent = response.complexity.toLocaleString();
+  
+      
+      const attemptsPerSecond = 1000000000;
+      const time = response.complexity / attemptsPerSecond;
+      document.querySelector('#time').textContent = time.toFixed(5);
+  
+      
+      const strengthBar = document.querySelector('#strengthBar');
+      const strengthText = document.querySelector('#strengthText'); 
+
+      let widthPercent = (time / 360000) * 100;
+      widthPercent = Math.min(widthPercent, 100); 
+      let percent = widthPercent.toFixed(5); 
+
+
+    let colorClass = 'bg-danger';
+    if (time > 3600000) {
+      colorClass = 'bg-success';
+    } else if (time > 6000) {
+     colorClass = 'bg-warning';
+    }
+
+
+    strengthBar.className = `progress-bar ${colorClass} progress-bar-striped progress-bar-animated`;
+    strengthBar.style.width = percent + '%';
+
+
+    strengthText.textContent = percent + '%';
+  });
+  
+
+   
+  
+ 
 
 
 
